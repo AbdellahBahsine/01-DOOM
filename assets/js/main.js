5874caee8745bf5a290f37eaf5126bb4aaf6c051
Paylo01 <paylo_@outlook.com>
2021-04-17 20:18:15 +0000

    started about section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
+const video = document.getElementById('video');
+const videoControls = document.getElementById('video-controls');
+const videoWorks = !!document.createElement('video').canPlayType;
+const playButton = document.getElementById('play');
+const playbackIcons = document.querySelectorAll('.playback-icons .fas');
+const timeElapsed = document.getElementById('time-elapsed');
+const duration = document.getElementById('duration');
+const progressBar = document.getElementById('progress-bar');
+const seek = document.getElementById('seek');
+const seekTooltip = document.getElementById('seek-tooltip');
+
+
 $(document).ready(function(){
     $(".owl-carousel").owlCarousel({
         items: 1,
@@ -16,5 +28,88 @@ $(document).ready(function(){
     const item = event.item.index + 1;    
    
    $('#counter').html("0"+item+"/0"+items)
- }
- 
+ } 
+
+  if (videoWorks) {
+    video.controls = false;
+    videoControls.classList.remove('hidden');
+  }
+
+ function togglePlay() {
+  if (video.paused || video.ended) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+playButton.addEventListener('click', togglePlay);
+
+function updatePlayButton() {
+  playbackIcons.forEach(playbackIcon => playbackIcon.classList.toggle('hidden'));
+}
+
+video.addEventListener('play', updatePlayButton);
+video.addEventListener('pause', updatePlayButton);
+
+function formatTime(timeInSeconds) {
+  const result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
+
+  return {
+    minutes: result.substr(3, 2),
+    seconds: result.substr(6, 2),
+  };
+};
+
+function initializeVideo() {
+  const videoDuration = Math.round(video.duration);
+  const time = formatTime(videoDuration);
+  duration.innerText = `${time.minutes}:${time.seconds}`;
+  duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+}
+
+video.addEventListener('loadedmetadata', initializeVideo);
+
+function updateTimeElapsed() {
+  const time = formatTime(Math.round(video.currentTime));
+  timeElapsed.innerText = `${time.minutes}:${time.seconds}`;
+  timeElapsed.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+}
+
+video.addEventListener('timeupdate', updateTimeElapsed);
+
+function initializeVideo() {
+  const videoDuration = Math.round(video.duration);
+  seek.setAttribute('max', videoDuration);
+  progressBar.setAttribute('max', videoDuration);
+  const time = formatTime(videoDuration);
+  duration.innerText = `${time.minutes}:${time.seconds}`;
+  duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+}
+
+function updateProgress() {
+  seek.value = Math.floor(video.currentTime);
+  progressBar.value = Math.floor(video.currentTime);
+}
+
+video.addEventListener('timeupdate', updateProgress);
+
+function updateSeekTooltip(event) {
+  const skipTo = Math.round((event.offsetX / event.target.clientWidth) * parseInt(event.target.getAttribute('max'), 10));
+  seek.setAttribute('data-seek', skipTo)
+  const t = formatTime(skipTo);
+  seekTooltip.textContent = `${t.minutes}:${t.seconds}`;
+  const rect = video.getBoundingClientRect();
+  seekTooltip.style.left = `${event.pageX - rect.left}px`;
+}
+
+seek.addEventListener('mousemove', updateSeekTooltip);
+
+function skipAhead(event) {
+  const skipTo = event.target.dataset.seek ? event.target.dataset.seek : event.target.value;
+  video.currentTime = skipTo;
+  progressBar.value = skipTo;
+  seek.value = skipTo;
+}
+
+seek.addEventListener('input', skipAhead);
